ca257168e79d223698629e25d5f4c8fc0ff07cae
CampbellHarry <hthebeast7@gmail.com>
2025-03-02 13:00:31 +0000

    Refactor heading check logic to improve text filtering and enhance content rendering

--- src/components/textuality/richtext.tsx
+++ src/components/textuality/richtext.tsx
@@ -2,20 +2,20 @@ import React, { useMemo } from 'react';
 
 export function CheckForHeadings(content: any) {
     const contenter = content?.content?.data?.merged?.filter((item: any) => item.type === 'Rich text')[0]?.value || content;
-    const h1Matches = contenter ? contenter.match(/<h1>(.*?)<\/h1>/g) : null;
-    const h2Matches = contenter ? contenter.match(/<h2>(.*?)<\/h2>/g) : null;
+    const h1Matches = contenter ? contenter.match(/<h1>([\s\S]*?)<\/h1>/g) : null;
+    const h2Matches = contenter ? contenter.match(/<h2>([\s\S]*?)<\/h2>/g) : null;
     
-    const h1Texts = h1Matches ? h1Matches.map((h1: string) => h1.replace(/<\/?h1>/g, '')) : [];
-    const h2Texts = h2Matches ? h2Matches.map((h2: string) => h2.replace(/<\/?h2>/g, '')) : [];
+    const h1Texts = h1Matches ? h1Matches.map((h1: string) => h1.replace(/<\/?h1>/g, '').replace(/<br\s*\/?>/g, ' ').replace(/\s+/g, ' ').trim()) : [];
+    const h2Texts = h2Matches ? h2Matches.map((h2: string) => h2.replace(/<\/?h2>/g, '').replace(/<br\s*\/?>/g, ' ').replace(/\s+/g, ' ').trim()) : [];
+    const filteredH1Texts = h1Texts.filter((text: string) => text.trim() !== '');
+    const filteredH2Texts = h2Texts.filter((text: string) => text.trim() !== '');
 
-    console.log(h1Texts, h2Texts);
-    
     const contentsHtml = `
         <div class='flex flex-col gap-1 border-b px-4 py-4 w-full h-full'>
             <h1 class="!mt-0 !pt-0 !text-[30px] !border-none !font-semibold">Contents</h1>
             <ul class="list-disc list-inside">
-                ${h1Texts.map((text: string) => `<li>${text}</li>`).join('')}
-                ${h2Texts.map((text: string) => `<li class="ml-4">${text}</li>`).join('')}
+                ${filteredH1Texts.map((text: string) => `<li>${text}</li>`).join('')}
+                ${filteredH2Texts.map((text: string) => `<li class="ml-4">${text}</li>`).join('')}
             </ul>
         </div>
     `;
@@ -25,9 +25,6 @@ export function CheckForHeadings(content: any) {
 
 export default function RichText({ content }: { content: any }) {
     const { contentsHtml } = useMemo(() => CheckForHeadings(content), [content]);
-
-    console.log(contentsHtml);
-    console.log(content);
 
     return (
         <div className='flex flex-col gap-1'>
